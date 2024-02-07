92aee2c8221a7fcd0f6824e31dcf396840e0bbe1
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-02-07 12:50:16 +0200

    Refactor board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/AbcSize
 # frozen_string_literal: true
 
 # token representation red_token = "\u{1F534}", yellow_token = "\u{1F7E1}"
@@ -20,21 +21,46 @@ class Board
   attr_accessor :board
 
   def initialize(board = [
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ' ', ' ', ' ', ' ', ' ', ' ', ' '
   ])
     @board = board
   end
 
-  def display
-    @board.each { |row| puts row.join(' | ') }
-
-    puts '1   2   3   4   5   6   7'
+  def display # rubocop:disable Metrics/MethodLength
+    puts "| #{board[36]} | #{board[37]} | #{board[38]} | #{board[39]} | #{board[40]} | #{board[41]} | #{board[42]}  |"
+    puts '|---+---+---+---+---+---+---|'
+    puts "| #{board[28]} | #{board[29]} | #{board[30]} | #{board[31]} | #{board[32]} | #{board[33]} | #{board[35]} |"
+    puts '|---+---+---+---+---+---+---|'
+    puts "| #{board[21]} | #{board[22]} | #{board[23]} | #{board[24]} | #{board[25]} | #{board[26]} | #{board[27]} |"
+    puts '|---+---+---+---+---+---+---|'
+    puts "| #{board[14]} | #{board[15]} | #{board[16]} | #{board[17]} | #{board[18]} | #{board[19]} | #{board[20]} |"
+    puts '|---+---+---+---+---+---+---|'
+    puts "| #{board[7]} | #{board[8]} | #{board[9]} | #{board[10]} | #{board[11]} | #{board[12]} | #{board[13]} |"
+    puts '|---+---+---+---+---+---+---|'
+    puts "| #{board[0]} | #{board[1]} | #{board[2]} | #{board[3]} | #{board[4]} | #{board[5]} | #{board[6]} |"
+    puts '|---+---+---+---+---+---+---|'
+    puts '| 1 | 2 | 3 | 4 | 5 | 6 | 7 |'
   end
 
-  def update(column, token); end
+  def update(column, token)
+    for i in (0..5)
+      next unless @board[(column + 7 * i) - 1] == ' '
+
+      @board[(column + 7 * i) - 1] = token
+      break
+
+    end
+  end
+
+  def valid?(column)
+    true if (1..7).includes?(column) && @board.each_index
+  end
+
+  def column_not_full?(column); end
 end
+# rubocop:enable Metrics/AbcSize
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,33 @@
 # frozen_string_literal: true
 
-require_relative './lib/board'
+require_relative '../lib/board'
 
 describe Board do
   describe '#update' do
+    subject(:board_update) { described_class.new }
     context 'When column is not full' do
-      it 'updates the column at the lowest spot'
+      it 'updates the column at the lowest spot' do
+        board_update.update(2, 'A')
+        expect(board_update.board[1]).to eq('A')
+      end
+
+      it 'updates the next spot up when lowest is full' do
+        board_update.update(2, 'A')
+        board_update.update(2, 'A')
+        expect(board_update.board[8]).to eq('A')
+      end
+    end
+  end
+
+  describe '#valid?' do
+    subject(:board_valid) { described_class.new }
+
+    it 'Returns true if column has available spots' do
+      expect(board_valid.valid?(column)).to be_truthy
+    end
+    it 'Returns false if column is full' do
+    end
+    it "Returns false if column doesn't exist" do
     end
   end
 end
